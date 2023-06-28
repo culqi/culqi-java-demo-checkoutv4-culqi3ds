5cf89c29e31ca4f52a945e79d80ecdf5509ae59e
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2023-06-28 09:47:22 -0500

    readme

--- src/main/resources/public/js/config/index.js
+++ src/main/resources/public/js/config/index.js
@@ -4,6 +4,8 @@ export default Object.freeze({
   CURRENCY: "PEN",
   PUBLIC_KEY: "pk_test_90667d0a57d45c48",
   COUNTRY_CODE: "PE",
+  RSA_ID: "",
+  RSA_PUBLIC_KEY: ""
 });
 
 export const customerInfo = {
